2089c425ce9059388b2fbc1829cdea78c4d82932
Pallabi Ghosh <7936914+pallabig@users.noreply.github.com>
2021-06-18 11:15:36 -0700

    small changes

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # LearningGraphsForGCN.code
 
-Requirements: Pytorch
+Requirements: 
+* Python 3.6
+* Pytorch 1.0
 
 Run: python train.py
 
 Cite: If you use this code please cite our paper "Learning Graphs for Knowledge Transfer with Limited Labels"
 
-This code is built on GCN code repos 1. https://github.com/tkipf/gcn.git 2. https://github.com/tkipf/pygcn.git
+This code is built on GCN code repos 
+1. https://github.com/tkipf/gcn.git 
+2. https://github.com/tkipf/pygcn.git
+
+The Soft Triple Loss code is borrowed from https://github.com/idstcv/SoftTriple.git
--- models.py
+++ models.py
@@ -2,18 +2,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 from layers import GraphConvolution
 
-
 class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
 
         self.gc1 = GraphConvolution(nfeat, nhid)
-        self.gc4 = GraphConvolution(nhid, nclass)
+        self.gc2 = GraphConvolution(nhid, nclass)
+
         self.dropout = dropout
 
     def forward(self, x, adj, A):
         x = F.relu(self.gc1(x, adj))
-        x1=x
+        x1 = x
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.gc4(x, A)
-        return x1,F.log_softmax(x, dim=1)
+        x = self.gc2(x, A)
+        return x1, F.log_softmax(x, dim=1)
