c42e421d58a81877f6a16f64b60b847ec48b4e4c
pallabig <7936914+pallabig@users.noreply.github.com>
2021-07-08 10:24:40 -0700

    minor change

--- train.py
+++ train.py
@@ -105,13 +105,11 @@ for epoch in range(args.epochs):
 
     loss_val = F.nll_loss(output[idx_val], labels[idx_val])
     acc_val = accuracy(output[idx_val], labels[idx_val])
-    acc_test = accuracy(output[idx_test], labels[idx_test])
     print('Epoch: {:04d}'.format(epoch+1),
           'loss_train: {:.4f}'.format(loss_train.item()),
           'acc_train: {:.4f}'.format(acc_train.item()),
           'loss_val: {:.4f}'.format(loss_val.item()),
           'acc_val: {:.4f}'.format(acc_val.item()),
-          'acc_test: {:.4f}'.format(acc_test.item()),
           'time: {:.4f}s'.format(time.time() - t))
 
     # Choose model at best validation accuracy
